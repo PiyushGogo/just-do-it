8034c7c6843b704b3ddfc94eebbb2b42f7375baa
PiyushGogo <140108425+PiyushGogo@users.noreply.github.com>
2024-11-05 23:52:31 +0530

    project structure

--- just-do-it/src/App.js
+++ just-do-it/src/App.js
@@ -1,12 +1,21 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import { Route, Routes } from "react-router-dom";
+import WhoWeAre from "./pages/WhoWeAre/WhoWeAre";
 
 function App() {
   return (
     <div className="App">
-      <h1>Header</h1>
-      <h1>home page</h1>
-      <h1>Footer</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/who-we-are" element={<WhoWeAre />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ just-do-it/src/pages/WhoWeAre/WhoWeAre.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WhoWeAre = () => {
+  return (
+    <div className="who-we-are">
+      <h1>WhoWeAre page</h1>{" "}
+    </div>
+  );
+};
+
+export default WhoWeAre;
