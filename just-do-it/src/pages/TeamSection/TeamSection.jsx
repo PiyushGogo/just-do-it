19fd62eb56f3be32a7c9cc294fd65ea19158e538
PiyushGogo <140108425+PiyushGogo@users.noreply.github.com>
2024-11-11 00:21:07 +0530

    Team section css fix

--- just-do-it/src/pages/TeamSection/TeamSection.jsx
+++ just-do-it/src/pages/TeamSection/TeamSection.jsx
@@ -7,56 +7,56 @@ const teamMembers = [
     name: "MANISH",
     role: "DIGITAL MARKETING & CREATIVE STRATEGIST",
     description:
-      "Passionate about blending strategy and creativity to bring brands to life through engaging digital experiences that connect and drive growth.",
+      "PASSIONATE ABOUT BLENDING STRATEGY AND CREATIVITY TO BRING BRANDS TO LIFE THROUGH ENGAGING DIGITAL EXPERIENCES THAT CONNECT AND DRIVE GROWTH.",
     image: "/Manish.webp",
   },
   {
     name: "MADHU",
     role: "SENIOR VIDEO EDITOR",
     description:
-      "Expert in Adobe Premiere Pro and Photoshop, with experience creating polished, professional videos for diverse industries.",
+      "EXPERT IN ADOBE PREMIERE PRO AND PHOTOSHOP, WITH OF EXPERIENCE CREATING POLISHED, PROFESSIONAL VIDEOS FOR DIVERSE INDUSTRIES.",
     image: "/Madhu.webp",
   },
   {
     name: "SOURAJIT",
     role: "SENIOR CONTENT WRITER & CREATIVE STRATEGIST",
     description:
-      "Expert in Adobe Premiere Pro and Photoshop, with experience creating polished, professional videos for diverse industries.",
+      "SKILLED IN DIFFERENT FORMS OF CONTENT WRITING INCLUDING SEO OPTIMIZED BLOGS, AD COPIES, AND CONTENT THAT SHINE ON SOCIAL PLATFORM. ALSO EXPERIENCED IN PERSONAL BRANDING.",
     image: "/Sourajit.webp",
   },
-  {
-    name: "MADHU",
-    role: "SENIOR VIDEO EDITOR",
-    description:
-      "Expert in Adobe Premiere Pro and Photoshop, with experience creating polished, professional videos for diverse industries.",
-    image: "/Madhu.webp",
-  },
+  // {
+  //   name: "MADHU",
+  //   role: "SENIOR VIDEO EDITOR",
+  //   description:
+  //     "Expert in Adobe Premiere Pro and Photoshop, with experience creating polished, professional videos for diverse industries.",
+  //   image: "/Madhu.webp",
+  // },
   {
     name: "PIYUSH",
     role: "JUNIOR VIDEO EDITOR",
     description:
-      "Expert in Adobe Premiere Pro and Photoshop, with experience creating polished, professional videos for diverse industries.",
+      "SKILLED IN AFTER EFFECTS, CAPCUT, AND FILMORA, ADDING A FRESH EDGE TO PROJECTS WITH DYNAMIC EFFECTS AND MOTION GRAPHICS.",
     image: "/Piyush.webp",
   },
   {
     name: "NIKHIL",
     role: "INFLUENCER MARKETING SPECIALIST",
     description:
-      "Expert in Adobe Premiere Pro and Photoshop, with experience creating polished, professional videos for diverse industries.",
+      "AN EXPERT IN CRAFTING AND EXECUTING CAMPAIGNS WITH INFLUENCERS TO ENHANCE BRAND REACH AND CREDIBILITY, UNIQUELY POSITIONING YOUR BRAND IN THE MARKET.",
     image: "/Nikhil.webp",
   },
   {
     name: "SAGAR",
     role: "PHOTOGRAPHER",
     description:
-      "Expert in Adobe Premiere Pro and Photoshop, with experience creating polished, professional videos for diverse industries.",
+      "CAPTURES HIGH-QUALITY VISUALS THAT ELEVATE ANY VIDEO OR SOCIAL MEDIA PROJECT, ADDING A UNIQUE STORYTELLING ELEMENT.",
     image: "/Sagar.webp",
   },
   {
     name: "PRAVEEN",
     role: "CONTENT SCRIPT SPECIALIST",
     description:
-      "Expert in Adobe Premiere Pro and Photoshop, with experience creating polished, professional videos for diverse industries.",
+      "DEDICATED TO REFINING AND ENHANCING SCRIPTS TO ENSURE CLARITY AND IMPACT, I CRAFT COMPELLING NARRATIVES THAT RESONATE WITH AUDIENCES AND ELEVATE BRAND MESSAGING.",
     image: "/Praveen.webp",
   },
 ];
