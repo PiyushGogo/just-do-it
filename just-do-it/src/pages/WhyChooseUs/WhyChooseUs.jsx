e083a845867ef81052d6df6a290c4143a692876a
PiyushGogo <140108425+PiyushGogo@users.noreply.github.com>
2024-11-11 12:35:12 +0530

    why choose us page css fix

--- just-do-it/src/pages/WhyChooseUs/WhyChooseUs.jsx
+++ just-do-it/src/pages/WhyChooseUs/WhyChooseUs.jsx
@@ -10,35 +10,34 @@ const WhyChooseUs = () => {
       </h1>
       <div className="bg">
         <div className="why-card">
-          <h2 className="text_heading"> DRIVEN BY PASSION</h2>
-          <p className="text_heading">
+          <h2 className="card_heading"> DRIVEN BY PASSION</h2>
+          <p className="card_desc">
             We’re a young, dynamic team fueled by genuine enthusiasm for our
-            work.Our passion ensures that Every project is approached with
+            work. Our passion ensures that every project is approached with
             creativity, energy, and commitment.
           </p>
         </div>
         <div className="why-card">
-          <h2 className="text_heading"> DRIVEN BY PASSION</h2>
-          <p className="text_heading">
-            We’re a young, dynamic team fueled by genuine enthusiasm for our
-            work.Our passion ensures that Every project is approached with
-            creativity, energy, and commitment.
+          <h2 className="card_heading">EAGER FOR CHALLENGES</h2>
+          <p className="card_desc">
+            We thrive on challenges and believe that each new project is an
+            opportunity to push our boundaries and innovate. Our adaptability
+            keeps us ready to embrace new ideas and deliver exceptional results.
           </p>
         </div>
         <div className="why-card">
-          <h2 className="text_heading"> DRIVEN BY PASSION</h2>
-          <p className="text_heading">
-            We’re a young, dynamic team fueled by genuine enthusiasm for our
-            work.Our passion ensures that Every project is approached with
-            creativity, energy, and commitment.
+          <h2 className="card_heading">DEDICATED TO EXCELLENCE</h2>
+          <p className="card_desc">
+            For us, this work is more than a job—it’s a calling. We give our all
+            to each project, putting in 100% effort and ensuring that every
+            detail aligns with your vision.
           </p>
         </div>
         <div className="why-card">
-          <h2 className="text_heading"> DRIVEN BY PASSION</h2>
-          <p className="text_heading">
-            We’re a young, dynamic team fueled by genuine enthusiasm for our
-            work.Our passion ensures that Every project is approached with
-            creativity, energy, and commitment.
+          <h2 className="card_heading">BUILT FOR YOUR SUCCESS</h2>
+          <p className="card_desc">
+            We’re here to make a difference for your brand, bringing our full
+            dedication and fresh perspective to every challenge and opportunity.
           </p>
         </div>
       </div>
